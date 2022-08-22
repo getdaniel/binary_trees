9da09b15e96e60b29ea764b13f4d0420fe537868
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-22 21:40:41 +0300

    insert left

--- binary_trees.h
+++ binary_trees.h
@@ -29,7 +29,7 @@ void binary_tree_print(const binary_tree_t *);
 
 /* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 
 
