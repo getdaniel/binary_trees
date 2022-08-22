62cf8ce996179f9da1c9a4ca0cad9c7f32316e1f
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-22 22:36:24 +0300

    delete

--- tests/3-main.c
+++ tests/3-main.c
@@ -14,10 +14,10 @@ int main(void)
 	root = binary_tree_node(NULL, 98);
 	root->left = binary_tree_node(root, 12);
 	root->right = binary_tree_node(root, 402);
-	binary_tree_print(root);
-	printf("\n");
 	binary_tree_insert_right(root->left, 54);
 	binary_tree_insert_right(root, 128);
 	binary_tree_print(root);
+	binary_tree_delete(root);
 	return (0);
 }
+
