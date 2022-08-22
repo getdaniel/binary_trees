84affb8f7fd805222efadcce3fcae82a71f552ef
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-22 21:18:08 +0300

    new node

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,13 +15,23 @@
 struct binary_tree_s
 {
 	int n;
-    	struct binary_tree_s *parent;
-    	struct binary_tree_s *left;
-    	struct binary_tree_s *right;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+/* Printing helper function */
+void binary_tree_print(const binary_tree_t *);
+
+/* Task function prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+
+
 
 #endif
