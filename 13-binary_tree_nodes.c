b9a31370df8ec370d8a85ed6059249bafab72acb
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-23 00:36:15 +0300

    fix betty error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
- * 
+ *
  * @tree: A pointer to the root node of the tree to count the number of nodes.
  *
  * Return: 0 if tree is NULL.
