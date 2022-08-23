171f84f1bceca626a5be14325cad7380479845ab
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-23 18:16:45 +0300

    AVL - Array to AVL

--- binary_trees.h
+++ binary_trees.h
@@ -77,7 +77,7 @@ bst_t *bst_remove(bst_t *root, int value);
 /* AVL tree tasks prototypes. */
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
-
+avl_t *array_to_avl(int *array, size_t size);
 
 
 
