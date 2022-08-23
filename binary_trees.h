8205d86380989a374dcd848d080eea28478a5d39
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-23 15:45:04 +0300

    is BST

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -65,7 +66,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-
+int binary_tree_is_bst(const binary_tree_t *tree);
 
 
 
