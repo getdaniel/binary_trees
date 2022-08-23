f5bfe6143be802c793b559053785006a00eb2088
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-23 14:10:05 +0300

    fix betty code error

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,7 +2,7 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int is_perfect_recursive(const binary_tree_t *tree,
-                size_t leaf_depth, size_t level);
+		size_t leaf_depth, size_t level);
 const binary_tree_t *get_leaf(const binary_tree_t *tree);
 size_t depth(const binary_tree_t *tree);
 unsigned char is_leaf(const binary_tree_t *node);
