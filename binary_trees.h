b3899aa27331acb9ca910a750aa7b772425381e7
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-23 21:43:46 +0300

    heap - extract

--- binary_trees.h
+++ binary_trees.h
@@ -85,7 +85,7 @@ avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
-
+int heap_extract(heap_t **root);
 
 
 
